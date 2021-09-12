5ef0732c46eec98b1b51f91ae3262ec032a4e91f
thanveeralisayed <thanveeralisayed@gmail.com>
2021-09-12 18:40:53 +0530

    prop types reusable

--- src/Components/Card/Post.tsx
+++ src/Components/Card/Post.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import { Card} from 'react-bootstrap';
 import Buttc from '../ButtonCom/Buttc';
+import { postProps } from '../Props/Props.type';
 
-
-type postProps = {
-    post: {
-        userId: Number,
-        id: Number,
-        title: String,
-        body: String
-    }
-}
-
-const Post = (props: postProps) => {
+const Post = ({ post }: postProps) => {
     return (
         <div>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Title>{props.post.title}</Card.Title>
+                    <Card.Title>{post.title}</Card.Title>
                     {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
                     <Card.Text>
-                       {props.post.body}
+                       {post.body}
                     </Card.Text>
                     <Buttc btnHandler={(event,id)=>{
                         console.log('btn clicked',event,id);
---
+++ src/Components/Props/Props.type.tsx
@@ -0,0 +1,11 @@
+type post = {
+    userId: Number,
+    id: Number,
+    title: String,
+    body: String
+}
+
+
+export type postProps = {
+    post: post
+}
